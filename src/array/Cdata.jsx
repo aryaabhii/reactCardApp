b432bc51e3c9954015e14577b27473f6318e4d8a
Abhijeet Kumar <aryaabhi84@gmail.com>
2023-01-09 23:50:41 +0530

    my first commit

---
+++ src/array/Cdata.jsx
@@ -0,0 +1,58 @@
+const Cdata = [
+    {
+        imgsrc: "https://cdn.pixabay.com/photo/2016/10/09/06/23/hacker-1725256__340.jpg",
+        sname: "Wallpaper-1",
+        title: "Free Hacker Wallpaper",
+        link: "https://cdn.pixabay.com/photo/2016/10/09/06/23/hacker-1725256__340.jpg",
+    },
+    {
+        imgsrc: "https://cdn.pixabay.com/photo/2017/05/17/15/08/ransomware-2321110__340.jpg",
+        sname: "Wallpaper-2",
+        title: "Free Hacker Wallpaper",
+        link: "https://www.google.com",
+    },
+    {
+        imgsrc: "https://cdn.pixabay.com/photo/2017/10/24/07/12/hacker-2883630__340.jpg",
+        sname: "Wallpaper-3",
+        title: "Free Hacker Wallpaper",
+        link: "https://cdn.pixabay.com/photo/2017/10/24/07/12/hacker-2883630__340.jpg",
+    },
+    {
+        imgsrc: "https://cdn.pixabay.com/photo/2017/05/17/15/08/ransomware-2321110__340.jpg",
+        sname: "Wallpaper-4",
+        title: "Free Hacker Wallpaper",
+        link: "https://www.google.com",
+    },
+    {
+        imgsrc: "https://cdn.pixabay.com/photo/2019/04/14/13/36/scam-4126798__340.jpg",
+        sname: "Wallpaper-5",
+        title: "Free Hacker Wallpaper",
+        link: "https://cdn.pixabay.com/photo/2019/04/14/13/36/scam-4126798__340.jpg",
+    },
+    {
+        imgsrc: "https://cdn.pixabay.com/photo/2019/04/29/09/33/anonymous-4165613__340.jpg",
+        sname: "Wallpaper-6",
+        title: "Free Hacker Wallpaper",
+        link: "https://cdn.pixabay.com/photo/2019/04/29/09/33/anonymous-4165613__340.jpg",
+    },
+    {
+        imgsrc: "https://cdn.pixabay.com/photo/2015/06/18/01/46/hack-813290__340.jpg",
+        sname: "Wallpaper-7",
+        title: "Free Hacker Wallpaper",
+        link: "https://cdn.pixabay.com/photo/2015/06/18/01/46/hack-813290__340.jpg",
+    },
+    {
+        imgsrc: "https://cdn.pixabay.com/photo/2017/10/24/07/12/hacker-2883630__340.jpg",
+        sname: "Wallpaper-8",
+        title: "Free Hacker Wallpaper",
+        link: "https://cdn.pixabay.com/photo/2017/10/24/07/12/hacker-2883630__340.jpg",
+    },
+    {
+        imgsrc: "https://cdn.pixabay.com/photo/2015/06/18/01/46/hack-813290__340.jpg",
+        sname: "Wallpaper-9",
+        title: "Free Hacker Wallpaper",
+        link: "https://cdn.pixabay.com/photo/2015/06/18/01/46/hack-813290__340.jpg",
+    },
+]
+
+export default Cdata;
